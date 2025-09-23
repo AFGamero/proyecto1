5eaaec9a637d0b7ddcda9c7321492e70a552bd56
AFGamero <andresgamero28sa@gmail.com>
2025-09-22 21:57:49 -0500

    nothing

--- src/main/java/com/unimag/repositories/PassengerProfile.java
+++ src/main/java/com/unimag/repositories/PassengerProfile.java
@@ -3,4 +3,6 @@ package com.unimag.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface    PassengerProfile extends JpaRepository<PassengerRepository, Long> {
+
+    //este no es necesario
 }
