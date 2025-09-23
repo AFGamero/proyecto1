1d943ae96abd71b79397f214a4ebba09cf3bff29
AFGamero <andresgamero28sa@gmail.com>
2025-09-23 15:46:00 -0500

    airport and booking test

---
+++ src/test/java/com/unimag/repositories/BookingItemRepositoryTest.java
@@ -0,0 +1,25 @@
+package com.unimag.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookingItemRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByBookingIdOrderBySegmentOrder() {
+    }
+
+    @Test
+    void calculateTotalByBookingId() {
+    }
+
+    @Test
+    void countSeatsSoldByFlightAndCabin() {
+    }
+}
