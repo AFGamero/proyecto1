db0256bce2cc696240848fac442581de2f1c0bf9
AFGamero <andresgamero28sa@gmail.com>
2025-09-23 15:26:39 -0500

    nuevos test

---
+++ src/test/java/com/unimag/repositories/SeatInventoryRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.unimag.repositories;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SeatInventoryRepositoryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findByFlightIdAndCabin() {
+    }
+
+    @Test
+    void hasMinimumSeatsAvailable() {
+    }
+}
