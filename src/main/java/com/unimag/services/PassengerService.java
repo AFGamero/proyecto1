5fb455d3f7966c0f22f33da8a9c2c52dc42bd389
AFGamero <andresgamero28sa@gmail.com>
2025-09-29 22:09:29 -0500

    modifications complete

--- src/main/java/com/unimag/services/PassengerService.java
+++ src/main/java/com/unimag/services/PassengerService.java
@@ -7,14 +7,14 @@ import com.unimag.api.dto.PassengerDtos.PassengerCreateRequest;
 
 import com.unimag.api.dto.PassengerDtos.PassengerCreateUpdateRequest;
 
-
-import java.awt.print.Pageable;
+import org.springframework.data.domain.Pageable;
 import java.util.List;
 
 public interface PassengerService {
 
     PassengerResponse create(PassengerCreateRequest request);
     PassengerResponse update(Long ID, PassengerCreateUpdateRequest request);
+    PassengerResponse getByEmail(String email);
     PassengerResponse getById(Long id);
 
     List<PassengerResponse> List(Pageable pagination);
--- src/main/java/com/unimag/services/PassengerServiceImpl.java
+++ src/main/java/com/unimag/services/PassengerServiceImpl.java
@@ -4,17 +4,57 @@ package com.unimag.services;
 import com.unimag.api.dto.PassengerDtos;
 import com.unimag.dominio.entidades.Passenger;
 import com.unimag.dominio.repositories.PassengerRepository;
+import com.unimag.services.mappers.PassengerMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.awt.print.Pageable;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Transactional
 public class PassengerServiceImpl  implements PassengerService {
+    @Override
+    public PassengerDtos.PassengerResponse create(PassengerDtos.PassengerCreateRequest request) {
+        Passenger saved = repo.save(PassengerMapper.toEntity(request));
+        return PassengerMapper.toResponse(saved);
+
+    }
+
+    @Override
+    public PassengerDtos.PassengerResponse update(Long ID, PassengerDtos.PassengerCreateUpdateRequest request) {
+       Passenger passenger = repo.findById(ID)
+               .orElseThrow(() -> new RuntimeException("Passenger with id " + ID + " not found"));
+        return null;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public PassengerDtos.PassengerResponse getByEmail(String email) {
+        return repo.findByEmailIgnoreCase(email).map(PassengerMapper::toResponse)
+                .orElseThrow(() -> new RuntimeException("Passenger with email " + email + " not found"));
+    }
+
+    @Override
+    public PassengerDtos.PassengerResponse getById(Long id) {
+        return repo.findById(id).map(PassengerMapper::toResponse)
+                .orElseThrow(() -> new RuntimeException("Passenger with id " + id + " not found"));
+    }
+
+    @Override
+    public List<PassengerDtos.PassengerResponse> List(org.springframework.data.domain.Pageable pagination) {
+        return repo.findAll(pagination).map(PassengerMapper::toResponse).;
+    }
+
+
+    @Override
+    public void delete(Long id) {
+
+    }
+
     private final PassengerRepository repo;
 
-    @Override public PassengerDtos.PassengerResponse create (PassengerDtos.PassengerCreateRequest req) {
-        Passenger passenger = repo.save();
-    }
+
 }
